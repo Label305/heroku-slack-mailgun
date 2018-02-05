1fa980d20239dc5f0c2761777aca9208a9cff2ca
Thijs Scheepers <thijs@label305.com>
2018-02-05 15:08:13 +0100

    mailgunwebhook

---
+++ index.js
@@ -0,0 +1,48 @@
+const PORT = process.env.PORT || 5000
+const url = '/api/mailgun';
+const apikey = process.env.MAILGUNAPIKEY;
+const hook = process.env.SLACKWEBHOOK;
+const channel = process.env.SLACKCHANNEL|| '#keeping-notify';
+
+if (!apikey) throw new Error('Missing mailgun api key');
+if (!hook) throw new Error('Missing slack hook url');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+var multipart = require('connect-multiparty');
+const SlackGun = require('node-slack-mailgun');
+
+const app = express();
+
+// parse application/json
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse multi-part/formdata
+app.use(multipart());
+
+const slackgun = SlackGun({
+    slack: { // Options for slack
+        hook, // Required. The hook URL for posting messages to slack.
+        channel, // Optional. By default we'll send to the #general channel.
+        options: 'object', // Optional. Options to pass to https://github.com/xoxco/node-slack#install-slack
+        username: 'Mailgun', // Optional. By default we'll send using the "Mailgun" username to slack.
+        icon_emoji: 'mailbox_with_mail', // Optional. By default we'll set the icon to :mailbox_with_mail:
+    },
+    mailgun: { // Options for mailgun
+        apikey, // Optional. Used for verifying the HMAC token sent with a request.
+    },
+    templates: {
+        opened: true,
+        clicked: true,
+        unsubscribed: true,
+        complained: true,
+        bounced: true,
+        dropped: true,
+        delivered: true,
+        all: true
+    }
+});
+
+app.use(url, slackgun);
+app.listen(PORT);
